90c00368332fc794b887c323ea009f8b234cbdb5
admin <admin@admins-Mac-mini.gateway>
2020-01-17 23:32:35 +1100

    update controllers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,28 @@
 var express = require("express");
-var burger = require("../models/burger");
+var burger = require("../models/burger.js");
 
-var router = require("router");
+var router = express.Router();
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burger: data
+        };
+
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("api/burgers", function(req, res) {
+    burger.insert([
+        "name", "devoured"
+    ], [
+        req.body.name, req.body.devoured
+    ], function(result) {
+        res.json({id: result.insertId});
+    });
+});
+
+
 
